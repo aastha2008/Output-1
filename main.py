841d05329e93926d91bf6682c4d3697de75789e5
Bhagat Aastha <109658@eesd.org>
2021-01-14 06:07:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a comment. I am going to print hello world   
+print("Hello world") 
+
+#Here is my personal info
+print("My name is Aastha Bhagat")
+print("My DOB is April 18, 2008")
+print("My favorite book is 'spider space web'")
+
+# This is Concat.   
+print("Aastha" + " Bhagat" + " is a good girl")
+print("Aastha Bhagat is a good girl")
